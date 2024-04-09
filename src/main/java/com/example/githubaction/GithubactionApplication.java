61e95669b69e27fc0c4f008441eb6aa3f7880a21
jwlim-ssts <jwlim@ssts.co.kr>
2024-04-09 17:00:57 +0900

    init commit

---
+++ src/main/java/com/example/githubaction/GithubactionApplication.java
@@ -0,0 +1,19 @@
+package com.example.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class GithubactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactionApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test(){
+        return ResponseEntity.ok().body("ok");
+    }
+}
---
+++ src/main/java/com/example/githubaction/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.githubaction.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity<?> test(){
+        return ResponseEntity.ok().body("ok");
+    }
+}
