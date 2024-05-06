c437f101581bfa2d4d864b66a6c4a912bb92ce7f
limjong-wan <lim8848@gmail.com>
2024-05-06 16:37:39 +0900

    deploy 추가

--- src/main/java/com/example/githubaction/controller/TestController.java
+++ src/main/java/com/example/githubaction/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.example.githubaction.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<?> test(){
+        log.info("test");
         return ResponseEntity.ok().body("ok");
     }
 }
