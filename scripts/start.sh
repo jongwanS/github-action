120aeff41e4f4b5684f957505a476381f0a1b55b
limjong-wan <lim8848@gmail.com>
2024-05-06 16:46:42 +0900

    deploy 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
+PROJECT_ROOT="/home/ubuntu/app/build/libs"
 JAR_FILE="$PROJECT_ROOT/githubaction-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
