dbd5bc9ce1906cc3573d9757365c63be8abc403c
limjong-wan <lim8848@gmail.com>
2024-05-06 16:42:49 +0900

    deploy 추가

--- src/main/java/com/example/githubaction/controller/TestController.java
+++ src/main/java/com/example/githubaction/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<?> test(){
-        log.info("test");
+        log.info("123");
         return ResponseEntity.ok().body("ok");
     }
 }
